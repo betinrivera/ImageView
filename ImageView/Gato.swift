e3d253416430578c4ff40b3660f7f4623269fe8f
Maestro <Maestro@Jennifer.local>
2018-08-28 18:43:44 -0700

    PrimeraVersion

---
+++ ImageView/Gato.swift
@@ -0,0 +1,17 @@
+//
+//  Gato.swift
+//  ImageView
+//
+//  Created by Maestro on 28/08/18.
+//  Copyright © 2018 Maestro. All rights reserved.
+//
+
+import Foundation
+
+class Gato {
+    var nombre : String?
+    var vidas : Int?
+    var edad : Int?
+    var esterilizado : Bool?
+    
+}
--- ImageView/ViewController.swift
+++ ImageView/ViewController.swift
@@ -10,9 +10,47 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imgSecuencia: UIImageView!
+    @IBOutlet weak var lblTexto: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        imgSecuencia.image = UIImage(named:"Carta2_00000")
+        
+        
+        var imagenes : [UIImage] = []
+        imagenes.append(UIImage(named: "Carta2_00000")!)
+        imagenes.append(UIImage(named: "Carta2_00001")!)
+        imagenes.append(UIImage(named: "Carta2_00002")!)
+        imagenes.append(UIImage(named: "Carta2_00003")!)
+        imagenes.append(UIImage(named: "Carta2_00004")!)
+        imagenes.append(UIImage(named: "Carta2_00005")!)
+        imagenes.append(UIImage(named: "Carta2_00006")!)
+        imagenes.append(UIImage(named: "Carta2_00007")!)
+
+        imgSecuencia.animationImages = imagenes
+        imgSecuencia.animationDuration = 0.2
+        
+        imgSecuencia.startAnimating()
+        
+        let miGato = Gato()
+        miGato.nombre = "Don Gato"
+        miGato.vidas = 6
+        
+        if miGato.nombre != nil {
+        let nombreGato : String = miGato.nombre!
+        
+        lblTexto.text = nombreGato
+        }
+        
+    }
+    @IBAction func doTapGatitu(_ sender: Any) {
+        lblTexto.text = "Meow Pariente!"
+        if imgSecuencia.isAnimating {
+            imgSecuencia.stopAnimating()
+        } else {
+            lblTexto.text = "Continua!"
+        }
     }
 
     override func didReceiveMemoryWarning() {
